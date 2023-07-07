0e2ee7263fb74cda24795fece764feb3ea734bbe
yosephog <thorfry@yahoo.com>
2023-07-07 11:59:34 +0200

    config server setup

---
+++ src/main/java/com/polarbookshop/catalogservice/repository/BookDataLoader.java
@@ -0,0 +1,25 @@
+package com.polarbookshop.catalogservice.repository;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+@RequiredArgsConstructor
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights",
+                "Lyra Silverstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey",
+                "Iorek Polarson", 12.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
